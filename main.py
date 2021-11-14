99ca4dcb219921631e24422bddf3fc0f25cc3dd3
NAWF ABDULLAH <92883149+NawfAbdullah@users.noreply.github.com>
2021-11-14 18:48:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ Bootstrap(app)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///portfolio.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
